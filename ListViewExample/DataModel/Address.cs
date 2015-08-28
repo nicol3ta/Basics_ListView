f8d90d11c2b38e612874072ba86ffb6e3839abd0
nicol3ta <nikk_mihali@yahoo.de>
2015-08-28 15:48:33 +0200

    Code for creating a ListView that reacts upon property change inside the ObservableCollection

---
+++ ListViewExample/DataModel/Address.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewExample.DataModel
+{
+    public class Address : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Id { get; set; }
+        
+        public string Name {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Street { get; set; }
+        
+        public string Number { get; set; }
+        
+        public string Code { get; set; }
+        
+        public string City { get; set; }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+
+        
+    }
+}
---
+++ ListViewExample/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using ListViewExample.DataModel;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using System.ComponentModel;
+using System.Collections.Specialized;
+
+namespace ListViewExample
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Address> AddressItems { get; set; }
+
+        public MainPage()
+        {
+            //I hardcoded two locations for this demo
+            Address address1 = new Address();
+            address1.City = "Munich";
+            address1.Name = "Hoover & Floyd";
+            Address address2 = new Address();
+            address2.City = "Munich";
+            address2.Name = "Lola Bar";
+            
+
+            AddressItems = new ObservableCollection<Address>();
+            AddressItems.CollectionChanged += ContentCollectionChanged;
+            AddressItems.Add(address1);
+            AddressItems.Add(address2);
+
+            this.InitializeComponent();
+            DataContext = this;
+            
+        }
+
+        /// <summary>
+        /// Registers and unregisters items inside collection for property changes. 
+        /// Added items will fire <seealso cref="addressChanged(object sender, PropertyChangedEventArgs e)">addressChanged</seealso> when a property was changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContentCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (Address item in e.OldItems)
+                {
+                    //Removed items
+                    item.PropertyChanged -= addressChanged;
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (Address item in e.NewItems)
+                {
+                    //Added items
+                    item.PropertyChanged += addressChanged;
+                }
+            }
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO React when an item is selected
+            Debug.WriteLine("Selection changed");
+        }
+
+        /// <summary>
+        /// Changes the name of the first item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Change the value of the Name property for the  
+            // first item in the list.           
+            AddressItems.FirstOrDefault().Name = "Killians Irish Pub";
+            
+         }
+
+        private void addressChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Debug.WriteLine("The property:'" + e.PropertyName + "' was changed");
+           
+        }
+    }
+    
+
+
+
+
+}
+
+
